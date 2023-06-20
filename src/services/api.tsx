ca8b21110672d5b4dde65edefbfc607dff3716f0
Jose <zimwolfgang24@gmail.com>
2023-06-20 12:24:53 -0400

    feat: frontend finished with api integration

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'http://127.0.0.1:8000/api/',
-    baseURL: 'https://api-django-challenge.onrender.com/api/',
+    baseURL: 'http://127.0.0.1:8000/api/',
+    // baseURL: 'https://api-django-challenge.onrender.com/api/',
 });
 
 export default api;
